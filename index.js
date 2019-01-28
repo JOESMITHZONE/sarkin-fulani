a290547c29f5918c959dd259ac325ecb1f742a85
nkenna <nkennannadi@gmail.com>
2019-01-28 15:07:18 +0100

    First commit

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.get('/', function(req, res) {
 
 
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || '3000', function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
