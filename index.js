de5e03c0e435a273558729f7d43588a19279b854
Edachejoel0000 <masterfriendly11@gmail.com>
2019-01-29 12:51:55 +0100

    First commit

--- index.js
+++ index.js
@@ -4,13 +4,31 @@ var path = require('path');
 const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+//app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//app.set('views', path.join(process.cwd() + '/views'));
+app.use(express.static(path.join(process.cwd() + '/public')));
+
+
+
+app.get('/', function (req, res) {
+ return res.sendFile(path.join(__dirname,'public', 'index.html'));
+ });
+
+
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
 
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
    // res.sendFile('index.html');
-    res.sendFile(path.join(__dirname + 'index.html'));
+    res.render('index.html');
 });
 
 
